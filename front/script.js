43217093c9059d3fc4b1da6f1945ac996042db22
Camila Piñero <cmlpr9@gmail.com>
2023-11-01 11:08:29 -0300

    agrega funcionalidad al boton edit de cuenta bancaria

--- front/script.js
+++ front/script.js
@@ -77,6 +77,23 @@ async function deleteCtaBancaria(id) {
 	}
 }
 
+async function putCtaBancaria(id, cuentaBancaria) {
+	try {
+		const response = await fetch(
+			`http://localhost:3000/ctaBancaria/${id}`,
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(cuentaBancaria),
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
 //falta function edit cta bancaria
 
 function armarTabla(datos, tablaHead, tablaBody, nombreTabla) {
@@ -107,16 +124,7 @@ function armarTabla(datos, tablaHead, tablaBody, nombreTabla) {
 
 		if (tablaBody) {
 			const opciones = document.createElement("th");
-			opciones.innerHTML = `<button class="btn btn-sm delete" id="delete-${d.id}">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"  >
-					<title>delete-outline</title>
-					<path
-						d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z"
-					/>
-				</svg>
-			</button>
+			opciones.innerHTML = `
 			<button class="btn btn-sm edit" id="edit-${d.id}" data-bs-toggle="modal"
 			data-bs-target="#${nombreTabla}Modal"> 
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -125,7 +133,17 @@ function armarTabla(datos, tablaHead, tablaBody, nombreTabla) {
 					d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z"
 				/>
 			</svg>
-		</button>`;
+		</button>
+		<button class="btn btn-sm delete" id="delete-${d.id}">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"  >
+					<title>delete-outline</title>
+					<path
+						d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z"
+					/>
+				</svg>
+			</button>`;
 			tr.appendChild(opciones);
 			tablaBody.appendChild(tr);
 		}
